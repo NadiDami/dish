b3be295faf1ae17be788312399892eb440feb765
Nadia Odunayo <nodunayo@gmail.com>
2013-11-29 14:11:25 +0000

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dish::Application.config.secret_key_base = '143aac274fbebc737868c8fa8616742c4319c46ac73d672f712ccaa21454fbf6dbbd5a6ee96994d4e3105adfa502649fc9f0ccbd0fd08a30f081ec0808b1cda7'
---
+++ features/homepage.feature
@@ -0,0 +1,9 @@
+Feature: The Dish Homepage
+        Given I am a user of the website
+        I need some kind of user interface
+
+        Background: The homepage
+                Given I am on the homepage
+
+        Scenario: The title
+                Then I should see "Dish"
